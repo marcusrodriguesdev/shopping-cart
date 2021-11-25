0891134fa53c02261a8d062e39258e68953d8a99
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-25 19:12:48 -0300

    shopping cart

---
+++ src/pages/ShoppingCart/index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { GlobalStyles } from '@material-ui/styled-engine';
+import AppBar from '@mui/material/AppBar';
+import {
+  Container, Link, Toolbar, Typography, CssBaseline,
+} from '@mui/material';
+import CardProduct from '../../components/CardProduct';
+
+function ShoppingCart() {
+  return (
+    <React.Fragment>
+      <GlobalStyles styles={{
+        ul: {
+          margin: 0, padding: 0, listStyle: 'none', fontFamily: 'roboto',
+        },
+      }}
+      />
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+            fontWeigth="700"
+          >
+            M2 Center
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#aa"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Internet
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#tv"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Televisão
+            </Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+        <Typography
+          component="h1"
+          variant="h4"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          Produtos
+        </Typography>
+      </Container>
+      <Container component="main" maxWidth="md">
+        <Typography
+          component="h1"
+          variant="h5"
+          align="left"
+          color="#A600FF"
+
+        >
+          Internet
+        </Typography>
+        <Typography
+          component="p"
+          fontSize="18px"
+          align="left"
+          color="gray"
+          margin="0 0 25px 0"
+        >
+          Selecione um plano de Internet para continuar
+        </Typography>
+        <CardProduct title="100MB" />
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default ShoppingCart;
