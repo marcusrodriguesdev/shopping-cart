1a000e030f645d7d65f6d76610dd901120833e8a
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-26 11:08:04 -0300

    add provider e add request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Provider from './context/Provider';
 import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
-    <>
+    <Provider>
       <ShoppingCart />
-    </>
+    </Provider>
   );
 }
 
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,9 +1,18 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import requestPlanInternet from '../services';
 import Context from './Context';
 
 function Provider({ children }) {
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchApi() {
+      const response = await requestPlanInternet();
+      setData(response);
+    }
+    fetchApi();
+  }, []);
 
   const context = {
     data,
