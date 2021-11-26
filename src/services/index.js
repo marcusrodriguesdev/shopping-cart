a64d7c31572e7f144b6fd1862b8aa0a1b0f0e316
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-26 11:07:43 -0300

    create request api

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const URL_INTERNET = 'https://my-json-server.typicode.com/marcusrodriguesdev/shopping-cart/internet';
+
+async function requestPlanInternet() {
+  const requiriment = await fetch(URL_INTERNET)
+    .then((resp) => resp.json());
+  return requiriment;
+}
+
+export default requestPlanInternet;
